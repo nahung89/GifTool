c55502d6696e5fc0c00a5bef3befda702350454e
Nah <nahung89@gmail.com>
2017-12-14 17:10:49 +0700

    1st source code

---
+++ GifTool/GenerateViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GenerateViewController.swift
+//  GifTool
+//
+//  Created by Nah on 12/14/17.
+//  Copyright © 2017 Nah. All rights reserved.
+//
+
+import UIKit
+
+class GenerateViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ GifTool/ListSourceController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GifTool
+//
+//  Created by Nah on 12/14/17.
+//  Copyright © 2017 Nah. All rights reserved.
+//
+
+import UIKit
+
+class ListSourceController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
